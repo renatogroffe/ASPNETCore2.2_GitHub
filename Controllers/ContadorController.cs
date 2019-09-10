e02a50b669e755774e2b3b7264548b6ca9b619bf
Renato Groffe <renato.groffe@yahoo.com.br>
2019-09-10 16:26:35 -0300

    ASP.NET Core 2.2 + API REST
    ASP.NET Core 2.2 + API REST

---
+++ Controllers/ContadorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APIContagem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+        private static string _FRAMEWORK;
+
+        static ContadorController()
+        {
+            _FRAMEWORK = Assembly
+                .GetEntryAssembly()?
+                .GetCustomAttribute<TargetFrameworkAttribute>()?
+                .FrameworkName;
+        }
+
+        [HttpGet]
+        public object Get([FromServices]IConfiguration configuration)
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Sistema = Environment.OSVersion.VersionString,
+                    Local =  "Notebook Renato",
+                    TargetFramework = _FRAMEWORK
+                };
+            }
+        }
+    }
+}
