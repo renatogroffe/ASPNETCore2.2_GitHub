97d5296c753f5754a4848bc9930d1a524a2eeed0
Renato Groffe <renato.groffe@yahoo.com.br>
2019-10-02 18:54:43 -0300

    Teste na Antlia
    Teste...

--- Controllers/ContadorController.cs
+++ Controllers/ContadorController.cs
@@ -35,7 +35,7 @@ namespace APIContagem.Controllers
                     Environment.MachineName,
                     Sistema = Environment.OSVersion.VersionString,
                     Hospedagem = "Azure App Service", // Alteração para testar o deployment automatizado
-                    Local =  "Treinamento Azure na Prática",
+                    Local =  "Antlia",
                     TargetFramework = _FRAMEWORK
                 };
             }
